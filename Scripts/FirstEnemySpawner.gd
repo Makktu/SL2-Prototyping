281fc0eb3ab03a4bd10ad01ad8370d71c9091405
Makktu <johnmcna@gmail.com>
2023-03-04 01:45:58 +0000

    starting enemy collisions

--- Scripts/FirstEnemySpawner.gd
+++ Scripts/FirstEnemySpawner.gd
@@ -1,23 +1,25 @@
 extends Node2D
 
 onready var basic_enemy = preload("res://Scenes/BasicEnemy1.tscn")
-var spawned = false
+var already_triggered = false
 var amount_spawned = 0
 
 
 func _on_Area2D_body_entered(body):
-	if !spawned:
-		$"/root/Global".under_threat()
-		spawned = true
-		start_spawning()
+	if already_triggered == true:
+		return
+	$"/root/Global".under_threat()
+	already_triggered = true
+	start_spawning()
 	
 
 func start_spawning():
+	if amount_spawned >= 5:
+		return
+	amount_spawned += 1
 	var loaded_enemy = basic_enemy.instance()
 	add_child(loaded_enemy)
-	if amount_spawned < 6:
-		amount_spawned += 1
-		$SpawnTimer.start()
+	$SpawnTimer.start()
 
 
 func _on_SpawnTimer_timeout() -> void:
