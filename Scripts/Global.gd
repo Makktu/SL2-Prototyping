3cbec76316ec4124dcd5a6d8457b511b4083f1eb
Makktu <johnmcna@gmail.com>
2023-03-08 20:21:21 +0000

    new environments experiment

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 var taking_damage = false
 
-var environments = ["res://Scenes/Environment_0.tscn"]
+var environments = ["res://Scenes/Environment_0.tscn", "res://Scenes/Environment_1.tscn"]
 
-var current_environment = 0
+var current_environment = 1
 
 onready var the_player = "res://Scenes/Player.tscn"
 
@@ -17,6 +17,9 @@ func get_player():
 	
 func basic():
 	$Basic.play()
+	
+func alt():
+	$Alt.play()
 	
 func under_threat():
 	$Basic.volume_db = -20
