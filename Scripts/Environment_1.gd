847b9719cccc48434a46849b502cc3a7289dec4c
Makktu <johnmcna@gmail.com>
2023-03-27 20:27:18 +0100

    progress at last

--- Scripts/Environment_1.gd
+++ Scripts/Environment_1.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 
 func _ready():
-	$"/root/Global".alt()
+	$"/root/Global".basic()
 
 
 func _on_Area2D_body_entered(body):
+	get_parent().get_child(0)._on_BG_body_entered(body)
+
+
+func _on_Environment_1_body_entered(body):
 	get_parent().get_child(0)._on_BG_body_entered(body)
