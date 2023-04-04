74cb9c336577d83a72e5badd6e545a82aef6ff5e
Makktu <johnmcna@gmail.com>
2023-04-04 03:08:44 +0100

    added music fade in and out

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
+onready var tween_out = get_node("Tween")
+onready var tween_in = get_node("Tween_In")
+var transition_duration = 5.00
+var transition_type = 1
 var taking_damage = false
-
 var environments = ["res://Scenes/Environment_0.tscn", "res://Scenes/Environment_1.tscn", "res://Scenes/Environment_3.tscn"]
-
 var current_environment = 2
-
 onready var the_player = "res://Scenes/Player.tscn"
-
 var enemies_chasing_player = 0
 var enemies_active = false
+#var current_music_position
 
 
 func _ready():
@@ -26,22 +27,35 @@ func alt():
 	
 func under_threat():
 	enemies_active = true
-	$Basic.volume_db = -20
+	fade_out($Basic)
 	$Threat.play()
-	for n in 500:
-		$Threat.volume_db += 0.15
-		if $Threat.volume_db == 0:
-			break
+	fade_in($Threat)
+
 	
 func _physics_process(delta):
 	if enemies_chasing_player == 0 && enemies_active:
 		enemies_active = false
-		for n in 3000:
-			$Threat.volume_db -= 0.01
-			if $Threat.volume_db == -80:
-				break
-		$Threat.volume_db = -80
-		$Threat.stop()
-		$Basic.volume_db = 0
+		fade_out($Threat)
+		$Basic.play()
+		fade_in($Basic)
+		
+		
+func fade_out(stream_player):
+#	current_music_position = $Basic.get_playback_position()
+	tween_out.interpolate_property(stream_player, "volume_db", 0, -80, transition_duration, transition_type, Tween.EASE_IN, 0)
+	tween_out.start()
+	
+	
+func fade_in(stream_player):
+	tween_in.interpolate_property(stream_player, "volume_db", -80, 0, transition_duration, transition_type, Tween.EASE_OUT, 0)
+	tween_in.start()
+	
+	
+func _on_Tween_tween_completed(object, key):
+	object.stop()
+	object.volume_db = 0
 
 
+#func _on_Tween_In_tween_completed(object, key):
+#	object.stop()
+#	object.volume_db = -80
